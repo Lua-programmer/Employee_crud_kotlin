1e3fb4ddca2e01e6dbbdff65fed4fa35b6278388
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-11 10:30:57 -0300

    Configure database

---
+++ src/main/kotlin/io/github/luaprogrammer/expat_dev/ExpatDevApplication.kt
@@ -0,0 +1,11 @@
+package io.github.luaprogrammer.expat_dev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExpatDevApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExpatDevApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/luaprogrammer/expat_dev/ExpatDevApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.luaprogrammer.expat_dev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExpatDevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
