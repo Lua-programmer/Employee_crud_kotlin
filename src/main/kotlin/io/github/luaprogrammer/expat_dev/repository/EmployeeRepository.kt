3c731a03d206174f3463082c50e118ef5e146e68
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-11 11:02:06 -0300

    Repository

---
+++ src/main/kotlin/io/github/luaprogrammer/expat_dev/repository/EmployeeRepository.kt
@@ -0,0 +1,8 @@
+package io.github.luaprogrammer.expat_dev.repository
+
+import io.github.luaprogrammer.expat_dev.model.Employee
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface EmployeeRepository: JpaRepository<Employee, Long>
